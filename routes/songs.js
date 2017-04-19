386cddd6b5342197581c5f3ea06a3c1fde3d4f0f
j-west <westja89@gmail.com>
2017-04-19 11:46:38 -0500

    Add import method to add new songs and add a route for that method

--- routes/songs.js
+++ routes/songs.js
@@ -5,10 +5,11 @@ const { Router } = require('express')
 const router = Router()
 
 // <require the songCtrl to get access to its methods>
-const { getSongs, getSong } = require('../controllers/songCtrl')
+const { getSongs, getSong, addSong } = require('../controllers/songCtrl')
 
 // <define routes for getting all songs and a single song>
 router.get('/songs', getSongs)
+router.post('/songs/add', addSong)
 router.get('/songs/:id', getSong)
 // <stretch: define routes for posting, deleting, editing a song>
 
