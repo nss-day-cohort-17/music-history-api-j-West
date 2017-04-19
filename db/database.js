99f5197c4783d1a80bf931cb4ab7b1c0055dd5ba
j-west <westja89@gmail.com>
2017-04-19 11:15:53 -0500

    Add database file contents to get knex/bookshelf configured

--- db/database.js
+++ db/database.js
@@ -1,7 +1,13 @@
 // <Setup the configuration module for knex and bookshelf>
-
 // I have already run 'knex init' at the root of this project to create the knexfile.js.
 // The music history database lives right here as a document in this project, so note
 // that the devlopment configuration sets that environment to use sqlite instead of postgres.
-
 // No need to make migrations, since the db and tables already exist
+const environment = 'development'
+const config = require('../knexfile')[environment]
+const knex = require('knex')(config)
+const bookshelf = require('bookshelf')(knex)
+
+bookshelf.plugin('registry')
+
+module.exports = {knex, bookshelf}
