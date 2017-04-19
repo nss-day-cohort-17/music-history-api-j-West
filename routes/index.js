2383cfc6147943fb184f5c54ee7906b0e31f2807
j-west <westja89@gmail.com>
2017-04-19 11:15:18 -0500

    Move the root json object to it's own file, added routes/songs content and required both files in routes/index

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 'use strict'
 
 // <require express Router and require the songs.js router file>
-
+const { Router } = require('express')
+const router = Router()
 
 // this is an example of a root document. It returns a json representation of
 // all of our endpoints. (Just one endpoint exists in this tiny example app)
-router.get('/', function(req, res) {
-  res.json({
-    "title": "The Music History API",
-    "songs": "http://localhost:3000/api/v1/songs",
-  });
-});
+router.use(require('./songs'))
+router.use(require('./rootEndpoints'))
+
+module.exports = router
---
+++ routes/rootEndpoints.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// <require the express Router>
+const { Router } = require('express')
+const router = Router()
+
+// <define routes for getting all songs and a single song>
+router.get('/', function (req, res, next) {
+  res.json({
+    'title': 'The Music History API',
+    'songs': 'http://localhost:3000/api/v1/songs',
+    'song': 'http://localhost:3000/api/v1/songs/:id'
+  })
+})
+
+module.exports = router
