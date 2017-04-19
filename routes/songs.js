0042e56627791c38eacc2304e9da66a4728b5a72
j-west <westja89@gmail.com>
2017-04-19 14:13:14 -0500

    Add editSong method and edit route to routes/index

--- routes/songs.js
+++ routes/songs.js
@@ -5,13 +5,15 @@ const { Router } = require('express')
 const router = Router()
 
 // <require the songCtrl to get access to its methods>
-const { getSongs, getSong, addSong, deleteSong } = require('../controllers/songCtrl')
+const { getSongs, getSong, addSong, deleteSong, editSong } = require('../controllers/songCtrl')
 
 // <define routes for getting all songs and a single song>
 router.get('/songs', getSongs)
-router.post('/songs/add', addSong)
 router.get('/songs/:id', getSong)
-router.delete('/songs/:SongId', deleteSong)
+
 // <stretch: define routes for posting, deleting, editing a song>
+router.post('/songs/add', addSong)
+router.delete('/songs/:SongId', deleteSong)
+router.post('/songs/:SongId', editSong)
 
 module.exports = router
