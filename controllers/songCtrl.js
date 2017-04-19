303e1aca01d30a075104d7d9a20f1abfdb4b94c9
j-west <westja89@gmail.com>
2017-04-19 11:17:02 -0500

    add songCtrl content to send data returned from the db to the broswer

--- controllers/songCtrl.js
+++ controllers/songCtrl.js
@@ -1,5 +1,28 @@
 'use strict'
 
 // <require a song model>
+// const { bookshelf } = require('../db/database')
+const Song = require('../models/song')
+
 // < use model methods for getting all songs and one song then send the response back with the data>
+module.exports.getSongs = (req, res, next) => {
+  Song.getAllSongs()
+  .then(songs => {
+    res.status(200).json(songs)
+  })
+  .catch(error => {
+    next(error)
+  })
+}
+
+module.exports.getSong = ({params: {id}}, res, next) => {
+  Song.getOneSong(id)
+  .then(song => {
+    res.status(200).json(song)
+  })
+  .catch(error => {
+    next(error)
+  })
+}
+
 // <stretch goal: methods for adding, deleting, editing a song>
